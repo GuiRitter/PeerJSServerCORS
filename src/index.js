2e73c847010736b565f45df36fc1030751724f8d
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2022-11-02 13:50:48 -0300

    attemp to make CORS work

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// CORS
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 const server = app.listen(process.env.PORT, '127.0.0.1').on('listening', () => {
 	console.log(`${(new Date()).toISOString()} are live on ${process.env.PORT}`);
 });
