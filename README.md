1ab7d5fdc27183541459aa3e93a2a61cc217f39d
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2022-11-02 08:10:02 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PeerJS Server with CORS
+
+I'm hosting a PeerJS Server instance to use in a project, and I've had to setup CORS so I can call it from any origin.
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import 'dotenv/config'; // always first import
+import express from 'express';
+import 'babel-polyfill';
+import cors from 'cors';
+
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+
+const peerServer = ExpressPeerServer(server, {
+	path: '/api'
+});
+
+// Add middleware for parsing URL encoded bodies (which are usually sent by browser)
+app.use(cors());
+// Add middleware for parsing JSON and urlencoded data and populating `req.body`
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/peerjs-server', peerServer);
+
+app.listen(process.env.PORT, '127.0.0.1').on('listening', () => {
+	console.log(`${(new Date()).toISOString()} are live on ${process.env.PORT}`);
+});
+
+export default app;
